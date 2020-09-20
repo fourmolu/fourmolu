862693ac16b65f4d66a53d693b52002071021c07
George Thomas <georgefsthomas@gmail.com>
2020-09-20 12:45:07 +0100

    Fix formatting script

--- format.sh
+++ format.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-export LANG="C.UTF-8"
-
 cabal format
 
-fourmolu -i $(find app -type f -name "*.hs")
-fourmolu -i $(find src -type f \( -name "*.hs" -o -name "*.hs-boot" \))
-fourmolu -i $(find tests -type f -name "*.hs")
+cabal run fourmolu -- -i $(find app -type f -name "*.hs")
+cabal run fourmolu -- -i $(find src -type f \( -name "*.hs" -o -name "*.hs-boot" \))
+cabal run fourmolu -- -i $(find tests -type f -name "*.hs")
