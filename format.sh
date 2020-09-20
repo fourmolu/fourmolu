941b5cc8bfaad9e8effe25f3fe9a3098ecc35f6c
George Thomas <georgefsthomas@gmail.com>
2020-09-20 12:52:31 +0100

    Avoid some recompilation

--- format.sh
+++ format.sh
@@ -4,6 +4,7 @@ set -e
 
 cabal format
 
-cabal run fourmolu -- -i $(find app -type f -name "*.hs")
-cabal run fourmolu -- -i $(find src -type f \( -name "*.hs" -o -name "*.hs-boot" \))
-cabal run fourmolu -- -i $(find tests -type f -name "*.hs")
+fourmolu -i \
+    $(find app -type f -name "*.hs") \
+    $(find src -type f \( -name "*.hs" -o -name "*.hs-boot" \)) \
+    $(find tests -type f -name "*.hs") \
