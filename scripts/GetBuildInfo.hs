f42c34eb2af9fd96bc6db114e179388d4a53527c
Brandon Chinn <brandonchinn178@gmail.com>
2022-05-16 22:31:41 -0700

    Set build-info in outputs

--- scripts/GetBuildInfo.hs
+++ scripts/GetBuildInfo.hs
@@ -1,9 +1,9 @@
 #!/usr/bin/env stack
-{- stack script --resolver=lts --package Cabal -}
+{- stack runghc --package Cabal -}
 
 import Data.Char (toLower)
 import Data.List (intercalate)
-import Distribution.Package (packageName, packageVersion, unPackageName)
+import Distribution.Package (packageVersion)
 import Distribution.PackageDescription.Parsec (readGenericPackageDescription)
 import Distribution.System (buildArch, buildOS)
 import qualified Distribution.Verbosity as Verbosity
@@ -12,8 +12,14 @@ import Distribution.Version (versionNumbers)
 main :: IO ()
 main = do
   packageDesc <- readGenericPackageDescription Verbosity.silent "fourmolu.cabal"
-  let name = unPackageName . packageName $ packageDesc
   let version = intercalate "." . map show . versionNumbers . packageVersion $ packageDesc
   let os = map toLower . show $ buildOS
   let arch = map toLower . show $ buildArch
-  putStrLn $ intercalate "-" [name, version, os, arch]
+  setOutput "version" version
+  setOutput "os" os
+  setOutput "arch" arch
+
+-- | Set output for a GitHub action.
+-- https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+setOutput :: String -> String -> IO ()
+setOutput name value = putStrLn $ "::set-output name=" ++ name ++ "::" ++ value
