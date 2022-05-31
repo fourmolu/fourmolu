88e96f6d3358055ad9ef0a7cd5918e12aabd239f
Brandon Chinn <brandonchinn178@gmail.com>
2022-05-31 01:23:13 -0700

    Add tests

---
+++ data/fourmolu/haddock-style/output-HaddockMultiLine-module=HaddockSingleLine.hs
@@ -0,0 +1,30 @@
+-- | This is a test multiline
+-- module haddock
+module Foo where
+
+-- | This is a singleline function haddock
+single1 :: Int
+
+-- | This is a singleline function haddock
+single2 :: Int
+
+{- | This is a multiline
+ function haddock
+-}
+multi1 :: Int
+
+{- |
+This is a multiline
+function haddock
+-}
+multi2 :: Int
+
+{- | This is a haddock
+
+ with two consecutive newlines
+
+
+ https://github.com/fourmolu/fourmolu/issues/172
+-}
+foo :: Int
+foo = 42
