8880b34f0d434c2fcfe69c646e1034a542498d14
Ignat Insarov <kindaro@gmail.com>
2022-11-08 14:03:19 +0300

    Add literal combinators for Unicode.

--- src/Ormolu/Printer/Combinators.hs
+++ src/Ormolu/Printer/Combinators.hs
@@ -64,6 +64,21 @@ module Ormolu.Printer.Combinators
     commaDel,
     commaDelImportExport,
     equals,
+    llarrowtail,
+    rrarrowtail,
+    darrow,
+    dcolon,
+    larrow,
+    larrowtail,
+    rarrow,
+    rarrowtail,
+    star,
+    forall,
+    oparenbar,
+    cparenbar,
+    openExpQuote,
+    closeQuote,
+    lolly,
 
     -- ** Stateful markers
     SpanMark (..),
@@ -349,6 +364,71 @@ commaDel' = \case
 -- | Print @=@. Do not use @'txt' "="@.
 equals :: R ()
 equals = interferingTxt "="
+
+-- The names of the following literals are from GHC's
+-- @compiler/GHC/Parser/Lexer.x@.
+
+llarrowtail,
+  rrarrowtail,
+  darrow,
+  dcolon,
+  larrow,
+  larrowtail,
+  rarrow,
+  rarrowtail,
+  star,
+  forall,
+  oparenbar,
+  cparenbar,
+  openExpQuote,
+  closeQuote,
+  lolly ::
+    R ()
+
+-- | Print @⤛@ or @-<<@ as appropriate.
+llarrowtail = "⤛" `whenUnicodeOtherwise` "-<<"
+
+-- | Print @⤜@ or @>>-@ as appropriate.
+rrarrowtail = "⤜" `whenUnicodeOtherwise` ">>-"
+
+-- | Print @⇒@ or @=>@ as appropriate.
+darrow = "⇒" `whenUnicodeOtherwise` "=>"
+
+-- | Print @∷@ or @::@ as appropriate.
+dcolon = "∷" `whenUnicodeOtherwise` "::"
+
+-- | Print @←@ or @<-@ as appropriate.
+larrow = "←" `whenUnicodeOtherwise` "<-"
+
+-- | Print @⤙@ or @-<@ as appropriate.
+larrowtail = "⤙" `whenUnicodeOtherwise` "-<"
+
+-- | Print @→@ or @->@ as appropriate.
+rarrow = "→" `whenUnicodeOtherwise` "->"
+
+-- | Print @⤚@ or @>-@ as appropriate.
+rarrowtail = "⤚" `whenUnicodeOtherwise` ">-"
+
+-- | Print @★@ or @*@ as appropriate.
+star = "★" `whenUnicodeOtherwise` "*"
+
+-- | Print @∀@ or @forall@ as appropriate.
+forall = "∀" `whenUnicodeOtherwise` "forall"
+
+-- | Print @⦇@ or @(|@ as appropriate.
+oparenbar = "⦇" `whenUnicodeOtherwise` "(|"
+
+-- | Print @⦈@ or @|)@ as appropriate.
+cparenbar = "⦈" `whenUnicodeOtherwise` "|)"
+
+-- | Print @⟦@ or @[|@ as appropriate.
+openExpQuote = "⟦" `whenUnicodeOtherwise` "[|"
+
+-- | Print @⟧@ or @|]@ as appropriate.
+closeQuote = "⟧" `whenUnicodeOtherwise` "|]"
+
+-- | Print @⊸@ or @%1 ->@ as appropriate.
+lolly = "⊸" `whenUnicodeOtherwise` "%1 ->"
 
 ----------------------------------------------------------------------------
 -- Placement
