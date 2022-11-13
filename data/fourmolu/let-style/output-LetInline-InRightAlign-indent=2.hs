962e05ef37fbf84747c7f8163a2395c9b8904ed8
Brandon Chinn <brandonchinn178@gmail.com>
2022-11-13 15:12:56 -0800

    Add test cases for current behavior

--- data/fourmolu/let-style/output-LetInline-InRightAlign-indent=2.hs
+++ data/fourmolu/let-style/output-LetInline-InRightAlign-indent=2.hs
@@ -26,6 +26,11 @@ let_single_sig =
       a = 1
    in a + 2
 
+let_single_comment =
+  let -- a comment
+      a = 1
+   in a + 2
+
 let_multi =
   let a = 1
       b = 2
@@ -57,6 +62,9 @@ test_do = do
   let d = "hello"
       e = "world"
    in print (d ++ e)
+
+  let f = 1
+   in print f
 
 {-- list comprehension --}
 
