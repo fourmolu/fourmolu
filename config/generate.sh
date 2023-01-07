d32da903aec2840cc6b3ad99099315b7144ff5b3
George Thomas <georgefsthomas@gmail.com>
2023-01-07 12:18:33 +0000

    Avoid using Dhall `--output` flag, for compatibility with Ubuntu CI

--- config/generate.sh
+++ config/generate.sh
@@ -5,5 +5,5 @@ set -eux -o pipefail
 HERE="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${HERE}"
 
-dhall text --file fourmolu_yaml.dhall --output ../fourmolu.yaml
-dhall text --file ConfigGen.dhall --output ../src/Ormolu/Config/Gen.hs
+dhall text --file fourmolu_yaml.dhall > ../fourmolu.yaml
+dhall text --file ConfigGen.dhall > ../src/Ormolu/Config/Gen.hs
