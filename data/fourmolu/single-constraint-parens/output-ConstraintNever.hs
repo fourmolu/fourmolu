e3adf6c0b1c2a38c3c896be8830e4b6f6468cd02
Brandon Chinn <brandonchinn178@gmail.com>
2023-09-22 19:12:52 -0700

    Fix single-constraint-parens for nested constraint

--- data/fourmolu/single-constraint-parens/output-ConstraintNever.hs
+++ data/fourmolu/single-constraint-parens/output-ConstraintNever.hs
@@ -5,3 +5,5 @@ functionName1 :: C a => a
 functionName2 :: (C a, D a) => a
 -- https://github.com/fourmolu/fourmolu/issues/340
 quantifiedConstraint :: (forall a. Show a => Show (f a)) => f Int
+-- https://github.com/fourmolu/fourmolu/issues/374
+nestedConstraint :: ((c a, c b) => c (f a b)) => Is c a -> Is c b -> Is c (f a b)
