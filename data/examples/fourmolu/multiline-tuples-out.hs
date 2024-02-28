758c9f2d5ba3277f643c161da82519bb3fb7c1cd
Brandon Chinn <brandonchinn178@gmail.com>
2024-02-27 19:33:59 -0800

    Add test case for multiline tuple arguments

---
+++ data/examples/fourmolu/multiline-tuples-out.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE PatternSynonyms #-}
+
+multilineFunction
+  ( a,
+    b,
+    c
+    ) = a + b + c
+
+multilineLambda =
+  \( a,
+     b,
+     c
+    ) -> a + b + c
+
+multilineCase = \case
+  ( a,
+    b,
+    c
+    ) -> a + b + c
+
+multilineDo = do
+  ( a,
+    b,
+    c
+    ) <-
+    _
+  pure $ a + b + c
+
+multilineQuasi =
+  [p|
+        ( a
+        , b
+        , c
+        )
+    |]
+
+-- top level
+( a,
+  b,
+  c
+  ) = (1, 2, 3)
+
+pattern MultilineSynonym =
+  ( 1,
+    2,
+    3
+    )
