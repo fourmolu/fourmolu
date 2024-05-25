371453f51309425ad8b940ad194b74d4959352a2
Borys Lykah <lykahb@fastmail.com>
2024-05-24 21:55:30 -0600

    Improve style for generate.sh

--- config/generate.sh
+++ config/generate.sh
@@ -5,11 +5,9 @@ set -eux -o pipefail
 HERE="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${HERE}"
 
-if command -v runghc &> /dev/null; then
-    runghc -- -Wall -Werror Generate.hs
-elif command -v stack &> /dev/null; then
-    stack runghc -- -Wall -Werror Generate.hs
-else
-    echo "runghc and stack could not be found"
-    exit 1
+RUNGHC=runghc
+if command -v stack &> /dev/null; then
+  RUNGHC="stack runghc"
 fi
+
+${RUNGHC} -- -Wall -Werror Generate.hs
