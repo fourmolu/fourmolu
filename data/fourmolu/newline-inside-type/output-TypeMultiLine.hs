e63325a3db4ecc751608879a404c01fa6965beba
Borys Lykah <lykahb@fastmail.com>
2024-05-26 18:15:57 -0600

    Add more examples to test suite

--- data/fourmolu/newline-inside-type/output-TypeMultiLine.hs
+++ data/fourmolu/newline-inside-type/output-TypeMultiLine.hs
@@ -1,24 +1,89 @@
-functionName ::
+oneLineParams ::
+    a -> b -> c -> d -> (a, b, c, d)
+multiLineParams ::
+    a ->
+    b ->
+    c ->
+    d ->
+    (a, b, c, d)
+forallOneLineParams ::
+    forall a b c d. a -> b -> c -> d -> (a, b, c, d)
+forallNewlineOneLineParams ::
+    forall a b c d.
+    a ->
+    b ->
+    c ->
+    d ->
+    (a, b, c, d)
+forallNewlineMultiLineParams ::
+    forall a b c d.
+    a ->
+    b ->
+    c ->
+    d ->
+    (a, b, c, d)
+constraintsNewlineOneLineParams ::
     (C1, C2, C3) =>
     a ->
     b ->
     c ->
     d ->
     (a, b, c, d)
-foo ::
-    (Show a, Eq a) =>
+constraintsNewlineMultiLineParams ::
+    (C1, C2, C3) =>
     a ->
-    a ->
-    ()
-bar ::
+    b ->
+    c ->
+    d ->
+    (a, b, c, d)
+-- Edge cases with several =>
+
+manyConstraintsNewlineOneLineParams ::
     (Show a) =>
     (Eq a) =>
     (Num a) =>
     a ->
     a ->
     ()
-contextBetweenParameters ::
+manyConstraintsNewlineMultiLineParams ::
     (Show a) =>
+    (Eq a) =>
+    (Num a) =>
+    a ->
+    a ->
+    ()
+manyConstraintsNewlineMultiLineParams ::
+    (Show a) =>
+    (Eq a) =>
+    (Num a) =>
+    a ->
+    a ->
+    ()
+-- The first occurence of the one-line parameters won't be preserved because
+-- they are a part of a larger multi-line span that goes till the end of the signature.
+constraintsOneLineParamsConstraintsOneLineParams ::
+    (Show a) =>
+    a ->
+    a ->
+    a ->
+    (Eq a) =>
+    (Num a) =>
+    a ->
+    a ->
+    ()
+constraintsMultiLineParamsConstraintsOneLineParams ::
+    (Show a) =>
+    a ->
+    a ->
+    a ->
+    (Eq a) =>
+    (Num a) =>
+    a ->
+    a ->
+    ()
+constraintsOneLineParamsConstraintsMultiLineParams ::
+    (Show a) =>
+    a ->
     a ->
     a ->
     (Eq a) =>
