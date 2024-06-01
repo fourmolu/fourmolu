30e9251fd7c66f98c75f7a94cee878bc5ebfa3f0
Brandon Chinn <brandonchinn178@gmail.com>
2024-06-01 15:25:47 -0700

    Add tests for config file discovery

--- tests/Ormolu/Integration/CLISpec.hs
+++ tests/Ormolu/Integration/CLISpec.hs
@@ -1,7 +1,7 @@
 module Ormolu.Integration.CLISpec (spec) where
 
 import Control.Monad (forM_)
-import Ormolu.Integration.Utils (getFourmoluExe, readProcess, readProcess')
+import Ormolu.Integration.Utils (ProcessSpec (..), getFourmoluExe, proc, readFrom, readProcess)
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath (takeDirectory, (</>))
 import System.IO.Temp (withSystemTempDirectory)
@@ -11,17 +11,65 @@ spec :: Spec
 spec =
   describe "Fourmolu CLI functionality" . beforeAll getFourmoluExe $ do
     it "formats stdin when no files specified" $ \fourmoluExe -> do
-      stdout <- readProcess' fourmoluExe ["--no-cabal"] unformattedCode
+      stdout <- readFrom $ (proc fourmoluExe ["--no-cabal"]) {procStdin = unformattedCode}
       stdout `shouldBe` formattedCode
 
     it "formats stdin when stdin specified" $ \fourmoluExe -> do
-      stdout <- readProcess' fourmoluExe ["--no-cabal", "-"] unformattedCode
+      stdout <- readFrom $ (proc fourmoluExe ["--no-cabal", "-"]) {procStdin = unformattedCode}
       stdout `shouldBe` formattedCode
+
+    it "finds config file in current directory when formatting stdin" $ \fourmoluExe ->
+      withTempDir $ \tmpdir -> do
+        writeFile' (tmpdir </> "fourmolu.yaml") "single-constraint-parens: never"
+        stdout <-
+          readFrom $
+            (proc fourmoluExe ["--no-cabal", "-"])
+              { procStdin = "f :: (Show a) => a -> String",
+                procCwd = Just tmpdir
+              }
+        stdout `shouldBe` "f :: Show a => a -> String\n"
+
+    it "finds config file in ancestor directory when formatting stdin" $ \fourmoluExe ->
+      withTempDir $ \tmpdir -> do
+        writeFile' (tmpdir </> "fourmolu.yaml") "single-constraint-parens: never"
+        let cwd = tmpdir </> "a" </> "b" </> "c"
+        createDirectoryIfMissing True cwd
+        stdout <-
+          readFrom $
+            (proc fourmoluExe ["--no-cabal", "-"])
+              { procStdin = "f :: (Show a) => a -> String",
+                procCwd = Just cwd
+              }
+        stdout `shouldBe` "f :: Show a => a -> String\n"
+
+    it "finds config file in directory of input file" $ \fourmoluExe ->
+      withTempDir $ \tmpdir -> do
+        writeFile' (tmpdir </> "fourmolu.yaml") "single-constraint-parens: never"
+        writeFile' (tmpdir </> "input.hs") "f :: (Show a) => a -> String"
+        stdout <-
+          readFrom $
+            (proc fourmoluExe ["--no-cabal", "input.hs"])
+              { procCwd = Just tmpdir
+              }
+        stdout `shouldBe` "f :: Show a => a -> String\n"
+
+    it "finds config file in XDG directory" $ \fourmoluExe ->
+      withTempDir $ \tmpdir -> do
+        let configDir = tmpdir </> "config"
+        writeFile' (configDir </> "fourmolu.yaml") "single-constraint-parens: never"
+        stdout <-
+          readFrom $
+            (proc fourmoluExe ["--no-cabal", "-"])
+              { procStdin = "f :: (Show a) => a -> String",
+                procCwd = Just tmpdir,
+                procExtraEnv = [("XDG_CONFIG_HOME", configDir)]
+              }
+        stdout `shouldBe` "f :: Show a => a -> String\n"
 
     it "recursively finds files in directories" $ \fourmoluExe -> do
       withTempDir $ \tmpdir -> do
         let hsFile = tmpdir </> "test.hs"
-        writeFile hsFile unformattedCode
+        writeFile' hsFile unformattedCode
 
         output <- readProcess fourmoluExe [tmpdir]
         output `shouldBe` formattedCode
@@ -37,8 +85,7 @@ spec =
                 tmpdir </> "dist-newstyle/test.hs"
               ]
         forM_ (testFiles ++ ignoredFiles) $ \fp -> do
-          createDirectoryIfMissing True (takeDirectory fp)
-          writeFile fp unformattedCode
+          writeFile' fp unformattedCode
 
         _ <- readProcess fourmoluExe ["-i", tmpdir]
         forM_ testFiles $ \fp -> do
@@ -49,5 +96,8 @@ spec =
           output `shouldBe` unformattedCode
   where
     withTempDir = withSystemTempDirectory "fourmolu-cli-test"
+    writeFile' fp s = do
+      createDirectoryIfMissing True (takeDirectory fp)
+      writeFile fp s
     unformattedCode = "main=print(1+1)\n"
     formattedCode = "main = print (1 + 1)\n"
--- tests/Ormolu/Integration/Utils.hs
+++ tests/Ormolu/Integration/Utils.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Ormolu.Integration.Utils
   ( getFourmoluExe,
     readProcess,
-    readProcess',
+    ProcessSpec (..),
+    proc,
+    readFrom,
   )
 where
 
 import Control.Monad (when)
 import System.Directory (findExecutable)
+import System.Environment (getEnvironment)
 import System.Exit (ExitCode (..))
-import System.Process (readProcessWithExitCode)
+import System.Process (CreateProcess (..), readCreateProcessWithExitCode)
+import System.Process qualified as Process
 
 -- | Find a `fourmolu` executable on PATH.
 getFourmoluExe :: IO FilePath
@@ -17,15 +23,39 @@ getFourmoluExe = findExecutable "fourmolu" >>= maybe (fail "Could not find fourm
 -- | Like 'System.Process.readProcess', except without specifying stdin and
 -- with better failure messages.
 readProcess :: FilePath -> [String] -> IO String
-readProcess cmd args = readProcess' cmd args ""
+readProcess cmd args = readFrom $ proc cmd args
 
--- | Like 'System.Process.readProcess', except with better failure messages.
-readProcess' :: FilePath -> [String] -> String -> IO String
-readProcess' cmd args stdin = do
-  (code, stdout, stderr) <- readProcessWithExitCode cmd args stdin
+data ProcessSpec = ProcessSpec
+  { procCmd :: FilePath,
+    procArgs :: [String],
+    procStdin :: String,
+    procCwd :: Maybe FilePath,
+    procExtraEnv :: [(String, String)]
+  }
+
+proc :: FilePath -> [String] -> ProcessSpec
+proc cmd args =
+  ProcessSpec
+    { procCmd = cmd,
+      procArgs = args,
+      procStdin = "",
+      procCwd = Nothing,
+      procExtraEnv = []
+    }
+
+readFrom :: ProcessSpec -> IO String
+readFrom ProcessSpec {..} = do
+  currentEnv <- getEnvironment
+  (code, stdout, stderr) <-
+    readCreateProcessWithExitCode
+      (Process.proc procCmd procArgs)
+        { cwd = procCwd,
+          env = Just $ procExtraEnv <> currentEnv
+        }
+      procStdin
 
   when (code /= ExitSuccess) $ do
-    putStrLn $ "Command failed: " ++ (unwords . map (\s -> "\"" ++ s ++ "\"")) (cmd : args)
+    putStrLn $ "Command failed: " ++ (unwords . map (\s -> "\"" ++ s ++ "\"")) (procCmd : procArgs)
     putStrLn "========== stdout =========="
     putStrLn stdout
     putStrLn "========== stderr =========="
