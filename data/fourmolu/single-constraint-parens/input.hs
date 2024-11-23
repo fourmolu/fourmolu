efdd5d1084d18b522b6a2b01831f788f9ac2b74d
Brandon Chinn <brandonchinn178@gmail.com>
2024-11-23 15:34:32 -0800

    Fix implicit params with single-constraint-parens (#447)

--- data/fourmolu/single-constraint-parens/input.hs
+++ data/fourmolu/single-constraint-parens/input.hs
@@ -9,3 +9,6 @@ quantifiedConstraint :: (forall a. Show a => Show (f a)) => f Int
 
 -- https://github.com/fourmolu/fourmolu/issues/374
 nestedConstraint :: ((c a, c b) => c (f a b)) => Is c a -> Is c b -> Is c (f a b)
+
+-- https://github.com/fourmolu/fourmolu/issues/446
+implicitParam :: (?a :: Int) => Int
