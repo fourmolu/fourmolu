758dd036b0f284893702cfb2e8060aa685a20b04
Brandon Chinn <brandonchinn178@gmail.com>
2024-11-23 15:32:46 -0800

    Allow multiline comments in indented contexts (#421)
    * Inline body helper
    
    * Allow multiline comments in indented contexts
    
    * Inline expressions
    
    * Ignore leading whitespace in haddocks when checking idempotence
    
    * Update examples

--- data/examples/fourmolu/multiline-haddocks-four-out.hs
+++ data/examples/fourmolu/multiline-haddocks-four-out.hs
@@ -1,12 +1,14 @@
 -- https://github.com/fourmolu/fourmolu/pull/64
 
 module Foo (
-    -- | Multiline comment
-    -- in header
+    {- | Multiline comment
+    in header
+    -}
     foo,
 ) where
 
 class Foo a where
-    -- | Multiline haddock
-    -- in class
+    {- | Multiline haddock
+    in class
+    -}
     multiline :: a
