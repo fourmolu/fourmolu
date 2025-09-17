2452c7070a50235df11866b38f6407a0e7c19a5d
kindl <kindl@users.noreply.github.com>
2025-09-17 20:53:55 +0200

    Config data

--- config/FourmoluConfig/ConfigData.hs
+++ config/FourmoluConfig/ConfigData.hs
@@ -90,6 +90,16 @@ allOptions =
         cliOverrides = emptyOverrides
       },
     Option
+      { name = "record-style",
+        fieldName = Just "poRecordStyle",
+        description = "How to place braces in records",
+        type_ = "RecordStyle",
+        default_ = HsExpr "RecordStyleAligned",
+        ormolu = HsExpr "RecordStyleAligned",
+        sinceVersion = Nothing,
+        cliOverrides = emptyOverrides
+      },
+    Option
       { name = "import-export-style",
         fieldName = Just "poImportExportStyle",
         description = "Styling of import/export lists",
