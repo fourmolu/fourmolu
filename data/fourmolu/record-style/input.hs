4bf7c74a6c25acb077e99d50490220f81687f29e
kindl <kindl@users.noreply.github.com>
2025-09-17 22:03:57 +0200

    Add test data

---
+++ data/fourmolu/record-style/input.hs
@@ -0,0 +1,16 @@
+data Foo = Foo
+    { a :: Int
+    , b :: Int
+    , c :: Int
+    }
+
+-- Test nesting
+data Client = PremiumClient { name :: String, address :: String, aFieldWithAnUnneccessarilyLongFieldName :: String }
+  -- And some comment here as well
+  | NotSoPremiumClient { name :: String, address :: String, aFieldWithAnUnneccessarilyLongFieldName :: String }
+
+x :: Foo -> [Int]
+x Foo { a = possiblyLongName, b = anotherLongName, c = longNameAsWell} = [possiblyLongName, anotherLongName, longNameAsWell]
+
+y :: Int -> Int -> Int -> Foo
+y possiblyLongName anotherLongName longNameAsWell = Foo { a = possiblyLongName, b = anotherLongName, c = longNameAsWell}
