6bca38692a4b90e68a87241db634cdcf1922dc47
Brandon Chinn <brandonchinn178@gmail.com>
2026-01-09 23:48:33 -0500

    Add dotslash config to release notes

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -2,7 +2,10 @@
 
 import argparse
 import itertools
+import json
+import subprocess
 from pathlib import Path
+from typing import Any, Mapping
 
 TOP = Path(__file__).resolve().parent.parent
 
@@ -22,10 +25,20 @@ def main() -> None:
     parser_generate = subparsers.add_parser("generate")
     parser_generate.add_argument("--output", "-o", type=Path)
     parser_generate.add_argument("--fourmolu-version", required=True)
+    parser_generate.add_argument("--bin.linux-x86_64", type=Path, required=True)
+    parser_generate.add_argument("--bin.linux-aarch64", type=Path, required=True)
+    parser_generate.add_argument("--bin.macos-x86_64", type=Path, required=True)
+    parser_generate.add_argument("--bin.macos-aarch64", type=Path, required=True)
     parser_generate.set_defaults(
         run=lambda args: generate(
             output=args.output,
             version=args.fourmolu_version,
+            binaries={
+                "linux-x86_64": getattr(args, "bin.linux_x86_64"),
+                "linux-aarch64": getattr(args, "bin.linux_aarch64"),
+                "macos-x86_64": getattr(args, "bin.macos_x86_64"),
+                "macos-aarch64": getattr(args, "bin.macos_aarch64"),
+            },
         )
     )
 
@@ -55,11 +68,52 @@ def get_changelog_for(version: str) -> str:
     )
 
 
-def generate(output: Path | None, version: str) -> None:
-    changelog = get_changelog_for(version)
+def generate(output: Path | None, version: str, binaries: Mapping[str, Path]) -> None:
+    release_notes = [
+        get_changelog_for(version),
+        "## [dotslash](https://dotslash-cli.com/docs/) config",
+        "```json",
+        "#!/usr/bin/env dotslash",
+        json.dumps(
+            get_dotslash_config(version, binaries),
+            indent=2,
+        ),
+        "```",
+    ]
 
     write = output.write_text if output else print
-    write(changelog)
+    write("\n".join(release_notes))
+
+
+BASE_DOWNLOAD_URL = "https://github.com/fourmolu/fourmolu/releases/download"
+
+
+def get_dotslash_config(version: str, binaries: Mapping[str, Path]) -> Any:
+    return {
+        "name": f"fourmolu-{version}",
+        "platforms": {
+            name: {
+                "size": bin_path.stat().st_size,
+                "hash": "sha256",
+                "digest": get_digest(bin_path),
+                "path": "fourmolu",
+                "providers": [
+                    {"url": f"{BASE_DOWNLOAD_URL}/v{version}/{bin_path.name}"},
+                ],
+            }
+            for name, bin_path in binaries.items()
+        },
+    }
+
+
+def get_digest(bin_path: Path) -> str:
+    proc = subprocess.run(
+        ["shasum", "-a", "256", bin_path],
+        check=True,
+        text=True,
+        stdout=subprocess.PIPE,
+    )
+    return proc.stdout.split()[0]
 
 
 if __name__ == "__main__":
