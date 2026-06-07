4486f7aa356df132498d0827fc556da6c1784db3
Brandon Chinn <brandonchinn178@gmail.com>
2026-06-07 22:52:43 +0200

    Regenerate tests

--- data/examples/declaration/value/function/pattern/or-patterns-four-out.hs
+++ data/examples/declaration/value/function/pattern/or-patterns-four-out.hs
@@ -20,7 +20,8 @@ case e of
 sane e = case e of
     1
     2
-    3 -> a
+    3 ->
+        a
     4
     5
     6 -> b
@@ -33,3 +34,10 @@ insane e = case e of
     (D; E (Just _) Nothing) ->
         4
     F -> 5
+
+food
+    foo@( A;
+            B;
+            C
+            ) = Just foo
+food _ = Nothing
