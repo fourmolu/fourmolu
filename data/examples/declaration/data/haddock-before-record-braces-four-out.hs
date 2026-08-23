a728a477385c369f39946c5130fd8610ef77837e
Brandon Chinn <brandonchinn178@gmail.com>
2026-08-23 12:21:34 -0700

    Add new test outputs

---
+++ data/examples/declaration/data/haddock-before-record-braces-four-out.hs
@@ -0,0 +1,6 @@
+module Example where
+
+data Hello = Hello
+    { hello :: String
+    -- ^ hello world
+    }
---
+++ data/examples/import/comment-between-merged-imports-four-out.hs
@@ -0,0 +1,6 @@
+import HscMain (newHscEnv)
+-- Implementations of the various modes
+import LoadIface (showIface)
+
+-- Imports for --abi-hash
+import LoadIface (loadUserInterface)
